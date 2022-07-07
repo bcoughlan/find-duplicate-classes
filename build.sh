d2e95c655dfab9b9d5b9aab7d37646efb70ffa55
Barry Coughlan <barry.coughlan@smartbear.com>
2022-07-07 13:20:18 +0100

    Java 8 support

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-javac DuplicateClasses.java
+javac -source 1.8 -target 1.8 DuplicateClasses.java
 rm -rf DuplicateClasses.jar
 jar cvfe DuplicateClasses.jar DuplicateClasses *.class
